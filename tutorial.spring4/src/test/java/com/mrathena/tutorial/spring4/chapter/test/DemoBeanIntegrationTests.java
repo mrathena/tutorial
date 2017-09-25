99b8b0cfcb09a1a2fd543947f571f776cec5dfa4
MRATHENA\mrathena <747779761@qq.com>
2017-09-25 20:29:03 +0800

    no message

---
+++ tutorial.spring4/src/test/java/com/mrathena/tutorial/spring4/chapter/test/DemoBeanIntegrationTests.java
@@ -0,0 +1,29 @@
+package com.mrathena.tutorial.spring4.chapter.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mrathena.tutorial.spring4.chapter3.test.TestBean;
+import com.mrathena.tutorial.spring4.chapter3.test.TestConfig;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = TestConfig.class)
+@ActiveProfiles("production	")
+public class DemoBeanIntegrationTests {
+
+	@Autowired
+	private TestBean testBean;
+	
+	@Test
+	public void prodBeanShouldInjectTest() {
+		String expected = "Production TestBean";
+		String actual = testBean.getContent();
+		Assert.assertEquals(expected, actual);
+	}
+	
+}
